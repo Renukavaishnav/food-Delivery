5c47969774bff74fffe47b8e1601153d3ecce916
renukaVaishnav <renuk7621@gmail.com>
2024-08-28 16:17:12 +0530

    website completed

--- slider.js
+++ slider.js
@@ -1,34 +1,34 @@
 let scriptindex = 1;
-showslides(scriptindex)
+showslides(scriptindex);
 
-function plusSlides(n){
-  showslides(scriptindex+=n);
+function plusSlides(n) {
+    showslides(scriptindex += n);
 }
 
-function currentSlide(n){
-    showslides(scriptindex+=n);
-
+function currentSlide(n) {
+    showslides(scriptindex = n);
 }
 
-function showslides(n){
+function showslides(n) {
     let i;
-    let slides = document.getElementsByClassName("mySlides");
+    let slides = document.getElementsByClassName("mySliders");
     let dot = document.getElementsByClassName("dot");
-    if(n>slides.length){
-        scriptindex=1
+    
+    if (n > slides.length) {
+        scriptindex = 1;
     }
-    if(n<1){
-        scriptindex=slides.length
+    if (n < 1) {
+        scriptindex = slides.length;
     }
 
-    for(i=0; i<slides.length; i++){
+    for (i = 0; i < slides.length; i++) {
         slides[i].style.display = 'none';
     }
 
-    for(i=0; i<dot.length; i++){
-        dots[i].className = dot[i].className.replace(" active ", "")
+    for (i = 0; i < dot.length; i++) {
+        dot[i].className = dot[i].className.replace(" active", "");
     }
-    
-    slides[scriptindex-1].style.display="block";
-    dots[scriptindex-1].classname+='active';
+
+    slides[scriptindex - 1].style.display = "block";
+    dot[scriptindex - 1].className += " active";
 }
